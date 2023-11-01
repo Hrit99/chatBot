71174f4792df27eed32019bd0ae9a6fa838a852f
star407 <starmatka786@gmail.com>
2023-11-01 17:48:48 +0530

    added js file

---
+++ index.js
@@ -0,0 +1,49 @@
+async function initializeChats() {
+    let chats = []
+    try{
+     resp = await fetch("https://chatbotopenai-en7k.onrender.com/allchats");
+     chats = await resp.json();
+    }
+    catch(error){
+      chats = []
+    }
+chats.forEach((chat) => {
+    if(chat.sender == "user"){
+        document.getElementById("chat").innerHTML += ` <div class="message sent">
+        <div class="message-content">${chat.content}</div>
+    </div>`
+    } else  if(chat.sender == "openai"){
+        document.getElementById("chat").innerHTML += `<div class="message received">
+        <div class="message-content">${chat.content}</div>
+    </div>`
+    }
+})
+  }
+
+
+const socket = io("https://chatbotopenai-en7k.onrender.com");
+const onSendClicked = ()=> {
+    let message = document.getElementById("message-input").value
+    if(message !== ""){
+        document.getElementById("chat").innerHTML += ` <div class="message sent">
+        <div class="message-content">${message}</div>
+    </div>`
+    document.getElementById("message-input").value = ""
+        socket.emit("usermessage", message)
+    }
+}
+
+
+socket.on('servermessage', async function (prompt) {
+    document.getElementById("chat").innerHTML += `<div class="message received">
+    <div class="message-content">${prompt}</div>
+</div>`
+  });
+
+
+  socket.on('servermessageerror', async function (prompt) {
+    document.getElementById("chat").innerHTML += `<div class="message received">
+    <div class="message-content">Netwrok error occured!!</div>
+</div>`
+  });
+
